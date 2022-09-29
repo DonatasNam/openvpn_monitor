6cb1f46d31c477a2c4e38c2d42d18c51814c8a7b
DonatasNam <79839997+DonatasNam@users.noreply.github.com>
2022-09-29 09:49:55 +0300

    Delete pass.sh

--- pass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $HOME/Documents/rutx_src
-make package/openvpn_monitor/{clean,compile} CONFIG_DEBUG=y V=s
-cd $HOME/Documents/rutx_src/bin/packages/arm_cortex-a7_neon-vfpv4/base
-sshpass -p Admin123 scp openvpn_monitor_1.0.0-1_arm_cortex-a7_neon-vfpv4.ipk root@192.168.1.1:/tmp
-
